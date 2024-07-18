41d0ff30e77a1db72e131768cdd4922207aeb91f
PaTish <pashatishinin@gmail.com>
2024-07-18 11:37:26 +0200

    project section

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ $('.wall').jaliswall({
     item : '.wall-item',
     columnClass : '.wall-column'
 });
+
+// portfolio filter
+var mixer = mixitup('.wall', {
+    selectors: {
+        target: '.wall-item'
+    },
+    animation: {
+        duration: 800
+    }
+});
