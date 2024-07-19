5728067fc3f44bda0ffd94404ce20a9afc803665
PaTish <pashatishinin@gmail.com>
2024-07-19 13:52:53 +0200

    next

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ $('.wall').jaliswall({
     columnClass : '.wall-column'
 });
 
+
+
+
+// typing effect
+var typingEffect = new Typed(".type_effect",{
+    strings : ["Designer","Python Developer","Frontend Developer"],
+    loop : true,
+    typeSpeed : 80,
+    backSpeed : 80,
+    backDelay : 1000
+});
+
 // portfolio filter
 var mixer = mixitup('.wall', {
     selectors: {
@@ -12,3 +24,18 @@ var mixer = mixitup('.wall', {
         duration: 800
     }
 });
+
+// scroll reveal
+ScrollReveal({
+    distance:"90px",
+    duration:2000,
+    delay:200,
+    reset: true,
+});
+
+ScrollReveal().reveal('h1,.text-animate, h2', { origin: 'top'});
+ScrollReveal().reveal('.buttons', { origin: 'left'});
+ScrollReveal().reveal('.wall-item, .cta-box', { origin: 'bottom'});
+     
+
+
