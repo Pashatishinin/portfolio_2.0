341fee73aa0860e8489c5829cf4b3db00bb8dfdb
PaTish <pashatishinin@gmail.com>
2024-07-25 18:23:08 +0200

    challenge adapte

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ var typingEffect = new Typed(".type_effect",{
     backSpeed : 80,
     backDelay : 1000
 });
+
+// active hamburger menu
+let menuIcon = document.querySelector(".menu-icon");
+let navlist = document.querySelector(".navlist");
+menuIcon.addEventListener("click",()=>{
+    menuIcon.classList.toggle("active");
+    navlist.classList.toggle("active");
+    document.body.classList.toggle("open");
+});
+
+// remove navbar
+navlist.addEventListener("click",()=>{
+    menuIcon.classList.remove("active");
+    navlist.classList.remove("active");
+    document.body.classList.remove("open");
+})
